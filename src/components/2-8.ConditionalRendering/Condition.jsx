d0af215002a3d0bc3bf5d2874633b5ca6ebe2195
한훈희 <hanjoi77@naver.com>
2021-11-05 15:31:29 +0900

    2-8.ConditionalRendering

---
+++ src/components/2-8.ConditionalRendering/Condition.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function UserGreeting(props) {
+  return <h1>{props.name && `${props.name},`} Welcome {props.count ? `It's ${props.count} time` : null}</h1>
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>
+}
+
+function Greeting(props) {
+  // if(props.isLoggedIn) {
+  //   <UserGreeting name="Jimmy" count={0} />
+  // }
+  // return <GuestGreeting />;
+  return props.isLoggedIn ? <UserGreeting name="Jimmy" count={0} /> : <GuestGreeting />
+}
+
+export default function Condition() {
+  const isLoggedIn = true;
+
+  return (
+    <div>
+      <Greeting isLoggedIn={isLoggedIn} />
+    </div>
+  )
+}
